a4c8afc9c450e043dc2e2d45384d171646b156f8
sergiotravassos <sergiotravassos@outlook.com>
2022-10-06 11:49:39 +0100

    Movies by genre

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,12 +1,15 @@
 package com.devsuperior.movieflix.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.services.MovieService;
 
 @RestController
@@ -16,10 +19,10 @@ public class MovieController {
 	@Autowired
 	private MovieService service;
 	
-	@GetMapping(value = "/id")
-	public ResponseEntity<MovieDTO> findMovie(){
-		MovieDTO dto = service.findMovie();
-		return ResponseEntity.ok().body(dto);
+	@GetMapping
+	public ResponseEntity<Page<MovieCardDTO>> findAllPaged(
+			@RequestParam(value = "genreId", defaultValue = "0") Long genreId, Pageable pageable) {
+		return ResponseEntity.ok().body(service.findMoviesPaged(genreId, pageable));
 	}
 	
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,15 +1,34 @@
 package com.devsuperior.movieflix.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 
 @Service
 public class MovieService {
 	
 	@Autowired
-	private MovieRepository repository;
+	private MovieRepository movieRepository;
 	
-
+	@Autowired
+	private GenreRepository genreRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieCardDTO> findMoviesPaged(Long genreId, Pageable pageable) {
+		List<Genre> genres = (genreId == 0) ? null : Arrays.asList(genreRepository.getOne(genreId));
+		Page<Movie> page = movieRepository.find(genres, pageable);
+		return page.map(MovieCardDTO::new);
+	}
+	
 }
